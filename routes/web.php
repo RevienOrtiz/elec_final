9138c2a2e063593b8cf923bc5927ecd236b18588
RevienOrtiz <burningblazeaov@gmail.com>
2025-05-05 13:48:33 +0800

    landing and sales

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landingpage');
 });
+use App\Http\Controllers\SalesController;
+
+Route::get('/sales-form', [SalesController::class, 'create'])->name('sales.create');
+Route::post('/sales-form', [SalesController::class, 'store'])->name('sales.store');
